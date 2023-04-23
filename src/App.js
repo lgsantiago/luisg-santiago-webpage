05a2daea18d67a07a71b959b15ec2fc7213d5736
Luis Santiago <luis.santiagoayala@gmail.com>
2023-04-23 19:53:41 -0400

    add header text and animations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './img/logo.png';
-import './App.css';
+import logo from "./img/logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <header className="header">
-        <div className='logo-box'>
+        <div className="logo-box">
           <img src={logo} alt="LG Santiago Logo" className="logo"></img>
         </div>
+
+        <div className="header-text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">LG Santiago</span>
+            <span className="heading-primary-sub">
+              Software Engineer | Developer Advocate
+            </span>
+          </h1>
+          <a href="http://localhost:3000/" className="btn btn-white">watch my videos</a>
+        </div>
+
       </header>
     </div>
   );
